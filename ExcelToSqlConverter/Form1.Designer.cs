f4c42d36f0f8281d2c8c1501781e3a014afe0795
PodiSodi <imogikyan@bk.ru>
2024-09-10 22:01:52 +0300

    Добавил возможность ввода символов переноса строки и табуляции в поля замены

--- ExcelToSqlConverter/Form1.Designer.cs
+++ ExcelToSqlConverter/Form1.Designer.cs
@@ -64,19 +64,19 @@
             // 
             // label1
             // 
-            label1.AutoSize = true;
             label1.Location = new Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new Size(91, 15);
+            label1.Size = new Size(144, 27);
             label1.TabIndex = 1;
             label1.Text = "Текущий файл:";
             // 
             // label2
             // 
             tableLayoutPanel1.SetColumnSpan(label2, 3);
-            label2.Location = new Point(3, 331);
+            label2.Dock = DockStyle.Fill;
+            label2.Location = new Point(3, 443);
             label2.Name = "label2";
-            label2.Size = new Size(52, 15);
+            label2.Size = new Size(1091, 20);
             label2.TabIndex = 3;
             label2.Text = "Пример";
             // 
@@ -86,9 +86,9 @@
             tableLayoutPanel1.SetColumnSpan(exampleLbl, 3);
             exampleLbl.Dock = DockStyle.Fill;
             exampleLbl.Font = new Font("Segoe UI", 2F, FontStyle.Regular, GraphicsUnit.Point);
-            exampleLbl.Location = new Point(3, 346);
+            exampleLbl.Location = new Point(3, 463);
             exampleLbl.Name = "exampleLbl";
-            exampleLbl.Size = new Size(954, 144);
+            exampleLbl.Size = new Size(1091, 192);
             exampleLbl.TabIndex = 4;
             exampleLbl.Text = "(),";
             // 
@@ -103,12 +103,13 @@
             fieldsTree.AllowDrop = true;
             tableLayoutPanel1.SetColumnSpan(fieldsTree, 2);
             fieldsTree.Dock = DockStyle.Fill;
-            fieldsTree.Location = new Point(3, 22);
+            fieldsTree.Location = new Point(3, 31);
+            fieldsTree.Margin = new Padding(3, 4, 3, 4);
             fieldsTree.Name = "fieldsTree";
             treeNode1.Name = "rootNode";
             treeNode1.Text = "Все поля";
             fieldsTree.Nodes.AddRange(new TreeNode[] { treeNode1 });
-            fieldsTree.Size = new Size(524, 260);
+            fieldsTree.Size = new Size(617, 347);
             fieldsTree.TabIndex = 13;
             fieldsTree.ItemDrag += fieldsTree_ItemDrag;
             fieldsTree.NodeMouseDoubleClick += fieldsTree_NodeMouseDoubleClick;
@@ -117,9 +118,10 @@
             // 
             // upBtn
             // 
-            upBtn.Location = new Point(3, 3);
+            upBtn.Location = new Point(3, 4);
+            upBtn.Margin = new Padding(3, 4, 3, 4);
             upBtn.Name = "upBtn";
-            upBtn.Size = new Size(75, 23);
+            upBtn.Size = new Size(86, 31);
             upBtn.TabIndex = 14;
             upBtn.Text = "/\\";
             upBtn.UseVisualStyleBackColor = true;
@@ -127,9 +129,10 @@
             // 
             // downBtn
             // 
-            downBtn.Location = new Point(3, 32);
+            downBtn.Location = new Point(3, 43);
+            downBtn.Margin = new Padding(3, 4, 3, 4);
             downBtn.Name = "downBtn";
-            downBtn.Size = new Size(75, 23);
+            downBtn.Size = new Size(86, 31);
             downBtn.TabIndex = 15;
             downBtn.Text = "\\/";
             downBtn.UseVisualStyleBackColor = true;
@@ -137,9 +140,10 @@
             // 
             // deleteBtn
             // 
-            deleteBtn.Location = new Point(3, 91);
+            deleteBtn.Location = new Point(3, 121);
+            deleteBtn.Margin = new Padding(3, 4, 3, 4);
             deleteBtn.Name = "deleteBtn";
-            deleteBtn.Size = new Size(75, 23);
+            deleteBtn.Size = new Size(86, 31);
             deleteBtn.TabIndex = 16;
             deleteBtn.Text = "X";
             deleteBtn.UseVisualStyleBackColor = true;
@@ -151,8 +155,8 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, addToolStripMenuItem, справкаToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Padding = new Padding(5, 2, 0, 2);
-            menuStrip1.Size = new Size(960, 24);
+            menuStrip1.Padding = new Padding(6, 3, 0, 3);
+            menuStrip1.Size = new Size(1097, 30);
             menuStrip1.TabIndex = 19;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -160,27 +164,27 @@
             // 
             файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { импортToolStripMenuItem, resetToolStripMenuItem });
             файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            файлToolStripMenuItem.Size = new Size(48, 20);
+            файлToolStripMenuItem.Size = new Size(59, 24);
             файлToolStripMenuItem.Text = "Файл";
             // 
             // импортToolStripMenuItem
             // 
             импортToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { excelToolStripMenuItem, csvToolStripMenuItem, manualToolStripMenuItem });
             импортToolStripMenuItem.Name = "импортToolStripMenuItem";
-            импортToolStripMenuItem.Size = new Size(118, 22);
+            импортToolStripMenuItem.Size = new Size(147, 26);
             импортToolStripMenuItem.Text = "Импорт";
             // 
             // excelToolStripMenuItem
             // 
             excelToolStripMenuItem.Name = "excelToolStripMenuItem";
-            excelToolStripMenuItem.Size = new Size(152, 22);
+            excelToolStripMenuItem.Size = new Size(188, 26);
             excelToolStripMenuItem.Text = "Excel...";
             excelToolStripMenuItem.Click += ImportExcel;
             // 
             // csvToolStripMenuItem
             // 
             csvToolStripMenuItem.Name = "csvToolStripMenuItem";
-            csvToolStripMenuItem.Size = new Size(152, 22);
+            csvToolStripMenuItem.Size = new Size(188, 26);
             csvToolStripMenuItem.Text = "CSV...";
             csvToolStripMenuItem.Click += ImportCsv;
             // 
@@ -188,14 +192,14 @@
             // 
             manualToolStripMenuItem.Enabled = false;
             manualToolStripMenuItem.Name = "manualToolStripMenuItem";
-            manualToolStripMenuItem.Size = new Size(152, 22);
+            manualToolStripMenuItem.Size = new Size(188, 26);
             manualToolStripMenuItem.Text = "Ручной ввод...";
             manualToolStripMenuItem.ToolTipText = "В разработке...";
             // 
             // resetToolStripMenuItem
             // 
             resetToolStripMenuItem.Name = "resetToolStripMenuItem";
-            resetToolStripMenuItem.Size = new Size(118, 22);
+            resetToolStripMenuItem.Size = new Size(147, 26);
             resetToolStripMenuItem.Text = "Сброс";
             resetToolStripMenuItem.Click += resetToolStripMenuItem_Click;
             // 
@@ -204,27 +208,27 @@
             addToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { copyMenuItem, guidMenuItem, unionMenuItem });
             addToolStripMenuItem.Enabled = false;
             addToolStripMenuItem.Name = "addToolStripMenuItem";
-            addToolStripMenuItem.Size = new Size(71, 20);
+            addToolStripMenuItem.Size = new Size(90, 24);
             addToolStripMenuItem.Text = "Добавить";
             // 
             // copyMenuItem
             // 
             copyMenuItem.Name = "copyMenuItem";
-            copyMenuItem.Size = new Size(276, 22);
+            copyMenuItem.Size = new Size(346, 26);
             copyMenuItem.Text = "Скопировать выделенный столбец";
             copyMenuItem.Click += copyMenuItem_Click;
             // 
             // guidMenuItem
             // 
             guidMenuItem.Name = "guidMenuItem";
-            guidMenuItem.Size = new Size(276, 22);
+            guidMenuItem.Size = new Size(346, 26);
             guidMenuItem.Text = "Столбец ГУИДов";
             guidMenuItem.Click += guidMenuItem_Click;
             // 
             // unionMenuItem
             // 
             unionMenuItem.Name = "unionMenuItem";
-            unionMenuItem.Size = new Size(276, 22);
+            unionMenuItem.Size = new Size(346, 26);
             unionMenuItem.Text = "Объединение/вычислимый столбец";
             unionMenuItem.Click += unionMenuItem_Click;
             // 
@@ -232,13 +236,13 @@
             // 
             справкаToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { оПрограммеToolStripMenuItem });
             справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
-            справкаToolStripMenuItem.Size = new Size(65, 20);
+            справкаToolStripMenuItem.Size = new Size(81, 24);
             справкаToolStripMenuItem.Text = "Справка";
             // 
             // оПрограммеToolStripMenuItem
             // 
             оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
-            оПрограммеToolStripMenuItem.Size = new Size(149, 22);
+            оПрограммеToolStripMenuItem.Size = new Size(187, 26);
             оПрограммеToolStripMenuItem.Text = "О программе";
             // 
             // fileNameLbl
@@ -247,17 +251,16 @@
             fileNameLbl.BackColor = SystemColors.InactiveBorder;
             fileNameLbl.BorderStyle = BorderStyle.FixedSingle;
             tableLayoutPanel1.SetColumnSpan(fileNameLbl, 2);
-            fileNameLbl.Dock = DockStyle.Fill;
-            fileNameLbl.Location = new Point(103, 0);
+            fileNameLbl.Location = new Point(153, 0);
             fileNameLbl.Name = "fileNameLbl";
-            fileNameLbl.Size = new Size(854, 19);
+            fileNameLbl.Size = new Size(941, 27);
             fileNameLbl.TabIndex = 20;
             fileNameLbl.Text = "Ничего не импортировано :'-(";
             // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.ColumnCount = 3;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.Controls.Add(label2, 0, 3);
@@ -268,8 +271,7 @@
             tableLayoutPanel1.Controls.Add(fieldsTree, 0, 1);
             tableLayoutPanel1.Controls.Add(exportBtn, 1, 2);
             tableLayoutPanel1.Dock = DockStyle.Fill;
-            tableLayoutPanel1.Location = new Point(0, 24);
-            tableLayoutPanel1.Margin = new Padding(3, 2, 3, 2);
+            tableLayoutPanel1.Location = new Point(0, 30);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 5;
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
@@ -277,7 +279,7 @@
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 35F));
-            tableLayoutPanel1.Size = new Size(960, 490);
+            tableLayoutPanel1.Size = new Size(1097, 655);
             tableLayoutPanel1.TabIndex = 21;
             // 
             // panel1
@@ -286,19 +288,19 @@
             panel1.Controls.Add(downBtn);
             panel1.Controls.Add(deleteBtn);
             panel1.Dock = DockStyle.Fill;
-            panel1.Location = new Point(533, 21);
-            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Location = new Point(626, 30);
             panel1.Name = "panel1";
-            panel1.Size = new Size(424, 262);
+            panel1.Size = new Size(468, 349);
             panel1.TabIndex = 21;
             // 
             // exportBtn
             // 
             exportBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             exportBtn.Enabled = false;
-            exportBtn.Location = new Point(421, 288);
+            exportBtn.Location = new Point(499, 386);
+            exportBtn.Margin = new Padding(3, 4, 3, 4);
             exportBtn.Name = "exportBtn";
-            exportBtn.Size = new Size(106, 40);
+            exportBtn.Size = new Size(121, 53);
             exportBtn.TabIndex = 6;
             exportBtn.Text = "Выгрузить...";
             exportBtn.UseVisualStyleBackColor = true;
@@ -306,20 +308,20 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(960, 514);
+            ClientSize = new Size(1097, 685);
             Controls.Add(tableLayoutPanel1);
             Controls.Add(menuStrip1);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            MinimumSize = new Size(650, 350);
+            Margin = new Padding(3, 4, 3, 4);
+            MinimumSize = new Size(740, 451);
             Name = "Form1";
             Text = "Files to SQL converter";
             FormClosed += Form1_FormClosed;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             tableLayoutPanel1.ResumeLayout(false);
-            tableLayoutPanel1.PerformLayout();
             panel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
